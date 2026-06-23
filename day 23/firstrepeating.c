9f356bcd68fe52088b7222fb54c4ffddfa684889
niyashapanwar350-stack <niyashapanwar350@gmail.com>
2026-06-23 17:49:33 +0530

    Add files via upload

---
+++ day 23/firstrepeating.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    int i,j;
+    printf("Enter a string:");
+    fgets(str,sizeof(str),stdin);
+
+    for(i=0;str[i]!= '\0';i++){
+        for(j=i+1;str[j]!= '\0';j++){
+            if(str[i]==str[j]){
+                printf("First repeating character:%c",str[i]);
+                return 0;
+            }
+        }
+    }
+    printf("No repeating character found.");
+}
---
+++ day 23/maximumoccuring.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    int freq[256]={0};
+    int i,max=0;
+    char ch;
+
+    printf("Enter a string:");
+    fgets(str,sizeof(str),stdin);
+
+    for(i=0;str[i]!= '\0';i++){
+        freq[(unsigned char)str[i]]++;
+
+    }
+    for(i=0;i<256;i++){
+        if(freq[i]>max){
+            max=freq[i];
+            ch=i;
+        }
+    }
+    printf("Maximum occuring character:%c",ch);
+    printf("\nFrequency:%d",max);
+}
